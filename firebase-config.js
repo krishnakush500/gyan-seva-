868877c33d4dd04a02d40bfec63d35bc0577648f
Krishna Kushwaha <kk5674010@gmail.com>
2026-06-27 13:24:33 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,19 +2,23 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
+// ✅ Firebase Config (Mobile + Web dono ke liye same)
 const firebaseConfig = {
-  apiKey: "AIzaSyAq1iPy7MHA_0MtnZnjHGSMq0Wfd9HlzDw",
-  authDomain: "gyan-seva-web.firebaseapp.com",
-  projectId: "gyan-seva-web",
-  storageBucket: "gyan-seva-web.firebasestorage.app",
-  messagingSenderId: "568662476769",
-  appId: "1:568662476769:web:a77176e0b8094d845c34a0"
+  apiKey: "AIzaSyCtrUQKvT-B66fXgxAF8yJA1e_9i98ddpA",
+  authDomain: "gyan-seva.firebaseapp.com",
+  projectId: "gyan-seva",
+  storageBucket: "gyan-seva.firebasestorage.app",
+  messagingSenderId: "451862212887",
+  appId: "1:451862212887:web:637fd8785c4c01bd448752",
+  measurementId: "G-R375P3YCHP"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
+const auth = getAuth(app);
 
-export { db, storage };
+export { db, storage, auth };
